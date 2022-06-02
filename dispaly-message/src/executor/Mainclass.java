156ad9123096f5a7b61ea34a23baa842ad9db09b
Admin <Admin@Admin-PC>
2022-06-02 16:08:34 +0530

    commiting

---
+++ dispaly-message/src/executor/Mainclass.java
@@ -0,0 +1,13 @@
+package executor;
+
+import service.Displaymessage;
+
+public class Mainclass {
+
+	public static void main(String[] args) {
+		Displaymessage d=new Displaymessage();
+		d.showMessage("Pooja");
+
+	}
+
+}
---
+++ dispaly-message/src/service/Displaymessage.java
@@ -0,0 +1,11 @@
+package service;
+
+public class Displaymessage {
+	public void showMessage(String name)
+	{
+		for(int i=1;i<=15;i++)
+		{
+			System.out.println("The name of student is:"+name);		}
+	}
+
+}
