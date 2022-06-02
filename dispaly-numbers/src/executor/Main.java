bb273195d4e783624bbf239784c607b8c1059fb8
Admin <Admin@Admin-PC>
2022-06-02 17:29:54 +0530

    commit

---
+++ dispaly-numbers/src/executor/Main.java
@@ -0,0 +1,14 @@
+package executor;
+
+import service.ShowName;
+
+public class Main {
+
+	public static void main(String[] args)
+	{
+		ShowName s=new ShowName();
+		s.dispaly("Lalita");
+
+	}
+
+}
---
+++ dispaly-numbers/src/service/ShowName.java
@@ -0,0 +1,12 @@
+package service;
+
+public class ShowName {
+	public void dispaly(String name)
+	{
+		for(int i=0;i<10;i++)
+		{
+			System.out.println("My name is:"+name);
+		}
+	}
+
+}
